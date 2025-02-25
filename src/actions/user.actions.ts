297cfb71a40ba6d42db6173f2094f7ec7ac8458b
Oorpow <309754513@qq.com>
2025-02-25 17:16:52 +0800

    init: 🎉

---
+++ src/actions/user.actions.ts
@@ -0,0 +1,9 @@
+import prisma from "@/lib/prisma";
+
+export async function syncUser() {
+    // await prisma.user.findUnique({
+    //     where: {
+            
+    //     }
+    // })
+}
---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import { SessionProvider } from "next-auth/react";
+
+function Dashboard() {
+    return (
+        <div>home</div>
+    );
+}
+
+export default Dashboard;
